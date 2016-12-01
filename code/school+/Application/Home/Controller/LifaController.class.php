7e418743fb55141d6cfd3d566a42b1ce434fd166
sunchiye <sunchiye20160201@gmail.com>
2016-12-01 16:29:17 +0800

    Add files via upload

---
+++ code/school+/Application/Home/Controller/LifaController.class.php
@@ -0,0 +1,50 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+
+class LifaController extends Controller
+{
+    protected $_db;
+   
+   //
+    public function dianpu(){
+        $_db=M('store_information');
+        $condition=array();        
+        $condition['s_type_id']=1;
+        $condition['si_id']=$_GET[si_id];
+        $result=$_db->where($condition)->select();
+        //$name = $result[0]['si_name'];
+        $this->assign('set', $result[0]);
+        $this->display();
+
+    }
+  
+    public function jianjie(){
+        $_db=M('store_information');
+        $condition=array();        
+        $condition['s_type_id']=1;
+        $condition['si_id']=$_GET[si_id];
+        $result=$_db->where($condition)->select();
+        $this->assign('set', $result[0]);
+        $this->display();
+    }
+    public function yuyue(){
+        $_db=M('store_information');
+        $condition=array();        
+        $condition['s_type_id']=1;
+        $condition['si_id']=$_GET[si_id];
+        $result=$_db->where($condition)->select();
+        $this->assign('set', $result[0]);
+        $this->display();
+    }
+    //理发列表显示正常
+    public function llist(){
+        $_db=M('store_information');
+        $condition=array();        
+        $condition['s_type_id']=1;        
+        $result=$_db->where($condition)->select();  
+        $this->assign('store', $result);
+        $this->display();
+    }
+}
